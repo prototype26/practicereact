60ccfcb97929ad7e435d6f34622f2cd6b7bfe6f3
mohammad akbar <akbardastgeer@gmail.com>
2022-10-23 19:39:45 +0530

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Btncomponent from './components/Uicomponents/Btncomponent';
+import {useState,useEffect} from 'react';
 function App() {
+
+const [name,setName] = useState("akbar")
+
+const Highordercomponent =(props)=><p>
+                               <Btncomponent param={props.names}/>
+                              </p>;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>this is app js</h1>
+      <input type="text" placeholder='enter text for btn text' onChange={(e)=>setName(e.target.value)}></input>
+      <Highordercomponent names={name}/>
     </div>
   );
 }
---
+++ src/components/Uicomponents/Btncomponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const btnComponent =(props)=>{
+    return(
+        <button>{props.param}</button>
+    )
+}
+
+export default btnComponent;
